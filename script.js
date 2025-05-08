ea70ce2cc28e9ebea77aaaba9bf56333d0a76a09
J-DELLOSA-2028 <jcdellosa@crc.pshs.edu.ph>
2025-05-08 13:21:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function registerForm() {
+  var name = document.getElementById("nameInput").value;
+  var purpose = document.getElementById("purposeInput").value;
+  var idNumber = document.getElementById("idNumberInput").value;
+  var section = document.getElementById("sectionInput").value;
+  var gender = document.getElementById("genderInput").value;
 
+  var queue = [];
+  var personInLine = "Name: " + name + " - Purpose: " + purpose + " - ID Number: " + idNumber + " - Section: " + idNumber + " - Gender: " + gender;
+  queue.shift(personInLine);
+  displayQueue();
+}
+
+function displayqueue() {
+	var show = "";
+	for (var i = 0; i < queue.length; i++){
+		show += queue[i] + "<br />";
+	}
+	document.getElementById("queue").innerHTML = show;
+}
